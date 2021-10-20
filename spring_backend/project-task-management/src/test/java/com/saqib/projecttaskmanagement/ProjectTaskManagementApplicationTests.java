a608adb4f07d953e150b4655fc3255d1b5e216bc
Saqib <saqib29111@gmail.com>
2021-10-20 15:55:19 +0600

    add spring backend fresh project

---
+++ spring_backend/project-task-management/src/test/java/com/saqib/projecttaskmanagement/ProjectTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saqib.projecttaskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
