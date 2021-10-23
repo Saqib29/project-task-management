c6570fa08af2085d18cc948a1391417d8047cec8
Saqib <saqib29111@gmail.com>
2021-10-23 15:03:29 +0600

    update User

--- spring_backend/project-task-management/src/main/java/com/saqib/projecttaskmanagement/service/UserService.java
+++ spring_backend/project-task-management/src/main/java/com/saqib/projecttaskmanagement/service/UserService.java
@@ -1,8 +1,9 @@
 package com.saqib.projecttaskmanagement.service;
 
 import com.saqib.projecttaskmanagement.model.User;
+import com.saqib.projecttaskmanagement.repository.UserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserService extends JpaRepository<User, Long> {
+public interface UserService extends UserRepository {
     public User findByEmail(String email);
 }
