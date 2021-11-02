48b6acf482839123c5d8a10a0a04d61d22fa612d
Saqib <saqib29111@gmail.com>
2021-11-03 01:37:58 +0600

    unfinished appbar

--- flutter_frontend/projecttaskmanagement/lib/views/widgets/appbar.dart
+++ flutter_frontend/projecttaskmanagement/lib/views/widgets/appbar.dart
@@ -4,8 +4,50 @@ class BaseAppBar {
 
   static getAppBar() {
     return AppBar(
-      title: Text("App Bar"),
+      titleSpacing: 50,
+      centerTitle: false,
+      toolbarHeight: 100,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('assets/lexoro_logo.png', height: 300, width: 150),
+            ),
+            
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("data"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("data"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("data"),
+                ),
+
+              ]
+            ),
+            // Text("Log Out"),
+
+            DropdownButton<String>(
+              items: <String>['A', 'B', 'C', 'D'].map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (_) {},
+            )
+
+          ],
+        ),
       elevation: 0,
+      
     );
   }
   
